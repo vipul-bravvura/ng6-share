1b81dae61a34541ae6c8b6a6b64a468e0c3ef07f
Vipul <vipul.s@bravvura.in>
2018-12-24 16:48:11 +0530

    push

---
+++ button/lib/share-button.module.d.ts
@@ -0,0 +1,5 @@
+import { ModuleWithProviders } from '@angular/core';
+import { ShareButtonsConfig } from '@ng6-share/core';
+export declare class ShareButtonModule {
+    static withConfig(config?: ShareButtonsConfig): ModuleWithProviders;
+}
---
+++ buttons/lib/share-buttons.module.d.ts
@@ -0,0 +1,5 @@
+import { ModuleWithProviders } from '@angular/core';
+import { ShareButtonsConfig } from '@ng6-share/core';
+export declare class ShareButtonsModule {
+    static withConfig(config?: ShareButtonsConfig): ModuleWithProviders;
+}
